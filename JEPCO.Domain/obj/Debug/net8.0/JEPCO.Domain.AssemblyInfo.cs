5f09028b7402659b045f69bac4d2c655d8d6b8ee
Mohamed Aly Eldin <malyeldin@fivectech.com>
2024-05-21 16:40:44 +0300

    add .git ignore

--- JEPCO.Domain/obj/Debug/net8.0/JEPCO.Domain.AssemblyInfo.cs
+++ JEPCO.Domain/obj/Debug/net8.0/JEPCO.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JEPCO.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae0745740e20f5972cbb796e179acb1aca7ba049")]
 [assembly: System.Reflection.AssemblyProductAttribute("JEPCO.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JEPCO.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
