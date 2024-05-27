88a761da625907ae5e1faafbb0bb2af79d56cc15
Mohamed Aly Eldin <malyeldin@fivectech.com>
2024-05-27 14:29:15 +0300

    Add query models abstractions

--- JEPCO.Shared/ModelsAbstractions/IQueryObject.cs
+++ JEPCO.Shared/ModelsAbstractions/IQueryObject.cs
@@ -4,6 +4,8 @@ namespace JEPCO.Shared.ModelsAbstractions;
 
 public interface IQueryObject
 {
+    string SortBy { get; set; }
+    bool IsAscending { get; set; }
     int Index { get; set; }
     int Size { get; set; }
 }
